664de7b1e7bc7bef9af3cf99da195fbf7d5c945d
giordano <giordano@gios-pc>
2023-11-14 20:47:07 -0500

    first commit

---
+++ include/Word.hpp
@@ -0,0 +1,16 @@
+#ifndef WORDL_HPP
+#define WORDL_HPP
+
+#include <string>
+#include <fstream>
+
+#define WORD_SIZE 5
+#define GUESSES_ALLOWED 6
+
+class Word 
+{
+private:
+
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "../include/Word.hpp"
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
+
